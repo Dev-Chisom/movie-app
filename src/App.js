21373af1dfac56096ff93836274689f3af02704f
Onwugbenu Chisom <onwugbenuglory@gmail.com>
2020-10-28 17:33:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+
+import Search from './component/Search';
+import Results from './component/Results';
 
 function App() {
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {}
+  });
+  const apiUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=36fc6aa5";
+
+  const search = (e) => {
+    if(e.key === "Enter"){
+      axios(apiUrl + "&s=" + state.s).then(({ data}) =>{
+        let results = data.Dearch;
+
+        setState(prevState => {
+          return{...prevState, results: results }
+        })
+      })
+    }
+  }
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    setState(prevState => {
+      return {...prevState, s: s}
+    });
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <header>
+        <h1>Movie Database</h1>
       </header>
+      <main>
+        <Search handleInput={handleInput} search={search}/>
+        <Results results={state.results}/>
+      </main>
     </div>
   );
 }
